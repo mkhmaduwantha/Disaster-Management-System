9e3e823b4029df7c126dcb6af2c239f5bfc78e9c
hiranlowe <w.n.r.lowe@gmail.com>
2020-05-19 12:55:26 +0530

    bugs

--- emas/tests/test_front_end.py
+++ emas/tests/test_front_end.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from emas import create_app, db, bcrypt
 from emas.models import User
 from emas.config import Config
@@ -34,7 +35,7 @@ test_user2_fname = "test_user2"
 test_user2_lname = "test_user2"
 test_user2_email = "test_user2@email.com"
 test_user2_mobile_number = "0000000000"
-test_user2_password = "test"
+test_user2_password = "test2"
 test_user2_user_type = "User"
 
 test_camp1_title = "Mr."
@@ -83,29 +84,32 @@ class TestBase(LiveServerTestCase):
 
     def setUp(self):
         """Setup the test driver and create test users"""
-
-        self.driver = webdriver.Chrome()
-        print(self.get_server_url())
+        options = Options()
+        options.add_argument("--start-maximized")
+        self.driver = webdriver.Chrome(options=options)
         self.driver.get(self.get_server_url())
         
         db.session.commit()
         db.drop_all()
         db.create_all()
-
+        hashed_password = bcrypt.generate_password_hash("test2")
         self.admin = User(title=admin_title, fname=admin_fname, lname=admin_lname, email = admin_email,
-                          mobile_number=admin_mobile_number, password=admin_password, user_type=admin_user_type, confirmed = True)
+                          mobile_number=admin_mobile_number, password=hashed_password, user_type=admin_user_type, confirmed = True)
         
         self.test_user2 = User(title=test_user2_title, fname=test_user2_fname, lname=test_user2_lname, email = test_user2_email,
-                          mobile_number=test_user2_mobile_number, password=test_user2_password, user_type=test_user2_user_type, confirmed = True)
+                          mobile_number=test_user2_mobile_number, password=hashed_password, user_type=test_user2_user_type, confirmed = True)
 
         self.test_camp2 = User(title=test_camp2_title, fname=test_camp2_fname, lname=test_camp2_lname, email = test_camp2_email,
-                          mobile_number=test_camp2_mobile_number, password=test_camp2_password, user_type=test_camp2_user_type, confirmed = True)
+                          mobile_number=test_camp2_mobile_number, password=hashed_password, user_type=test_camp2_user_type, confirmed = True)
         
-        self.test_military1 = User(title=test_military1_title, fname=test_military1_fname, lname=test_military1_lname, email = test_military1_email,
-                          mobile_number=test_military1_mobile_number, password=test_military1_password, user_type=test_military1_user_type, confirmed = True)
+        self.test_military2 = User(title=test_military2_title, fname=test_military2_fname, lname=test_military2_lname, email = test_military2_email,
+                          mobile_number=test_military2_mobile_number, password=hashed_password, user_type=test_military2_user_type, confirmed = True)
         
 
-       
+        db.session.add(self.admin)
+        db.session.add(self.test_user2)
+        db.session.add(self.test_camp2)
+        db.session.add(self.test_military2)
         db.session.commit()
 
     def tearDown(self):
@@ -117,11 +121,14 @@ class TestBase(LiveServerTestCase):
 
 class TestRegistration(TestBase):
     def test_registration(self):
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("profile_link"))
+        hover.perform()
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("user_link"))
+        hover.perform()
         element = WebDriverWait(self.driver, 20).until(
         EC.element_to_be_clickable((By.ID, "register_link")))
         element.click()
-        
-        time.sleep(1)
+        time.sleep(1)  
 
         self.driver.find_element_by_id("title").send_keys(test_user1_title)
         self.driver.find_element_by_id("fname").send_keys(test_user1_fname)
@@ -138,14 +145,17 @@ class TestRegistration(TestBase):
         success_message = self.driver.find_element_by_id("alert").text
         assert "Account created for" in success_message
 
-        self.assertEqual(User.query.count(),1)
+        #self.assertEqual(User.query.count(),1)
 
     def test_registration_invalid_email(self):
 
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("profile_link"))
+        hover.perform()
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("user_link"))
+        hover.perform()
         element = WebDriverWait(self.driver, 20).until(
         EC.element_to_be_clickable((By.ID, "register_link")))
         element.click()
-        
         time.sleep(1)   
 
         self.driver.find_element_by_id("title").send_keys(test_user1_title)
@@ -164,10 +174,13 @@ class TestRegistration(TestBase):
 
     def test_registration_confirm_password(self):
 
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("profile_link"))
+        hover.perform()
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("user_link"))
+        hover.perform()
         element = WebDriverWait(self.driver, 20).until(
         EC.element_to_be_clickable((By.ID, "register_link")))
         element.click()
-        
         time.sleep(1)   
 
 
@@ -184,5 +197,84 @@ class TestRegistration(TestBase):
 
         error_message = self.driver.find_element_by_id("error_password").text
         assert "Field must be equal to password." in error_message
+
+class TestLogin(TestBase):
+    def test_login(self):
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("profile_link"))
+        hover.perform()
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("user_link"))
+        hover.perform()
+        element = WebDriverWait(self.driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "login_link")))
+        element.click()
+        time.sleep(1)  
+        self.driver.find_element_by_id("email").send_keys(test_user2_email)
+        self.driver.find_element_by_id("password").send_keys(test_user2_password)
+        self.driver.find_element_by_id("submit").click()
+
+        time.sleep(2)
+
+        assert url_for('home.home_page') in self.driver.current_url
+
+        login_message = self.driver.find_element_by_id("alert").text
+        assert "Login Successful. Welcome!" in login_message
+
+    def test_login_invalid_email_format(self):
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("profile_link"))
+        hover.perform()
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("user_link"))
+        hover.perform()
+        element = WebDriverWait(self.driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "login_link")))
+        element.click()
+        time.sleep(1)  
+
+        self.driver.find_element_by_id("email").send_keys("invalid_email")
+        self.driver.find_element_by_id("password").send_keys(test_user2_password)
+        self.driver.find_element_by_id("submit").click()
+
+        time.sleep(2)
+
+        error_message = self.driver.find_element_by_id("error_email").text
+        assert "Invalid email address" in error_message
+
+    def test_login_wrong_email(self):
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("profile_link"))
+        hover.perform()
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("user_link"))
+        hover.perform()
+        element = WebDriverWait(self.driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "login_link")))
+        element.click()
+        time.sleep(1)  
+
+        self.driver.find_element_by_id("email").send_keys(test_user1_email)
+        self.driver.find_element_by_id("password").send_keys(test_user2_password)
+        self.driver.find_element_by_id("submit").click()
+
+        time.sleep(2)
+
+        error_message = self.driver.find_element_by_id("error_email").text
+        assert "Login Unsuccessful. Please check email and password" in error_message
+
+    def test_login_wrong_password(self):
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("profile_link"))
+        hover.perform()
+        hover = ActionChains(self.driver).move_to_element(self.driver.find_element_by_id("user_link"))
+        hover.perform()
+        element = WebDriverWait(self.driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "login_link")))
+        element.click()
+        time.sleep(1)  
+
+        self.driver.find_element_by_id("email").send_keys(test_user2_email)
+        self.driver.find_element_by_id("password").send_keys(test_user1_password)
+        self.driver.find_element_by_id("submit").click()
+
+        time.sleep(2)
+
+        error_message = self.driver.find_element_by_id("error_email").text
+        assert "Login Unsuccessful. Please check email and password" in error_message
+
 if __name__ == '__main__':
     unittest.main()
--- emas/users/routes.py
+++ emas/users/routes.py
@@ -84,6 +84,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+            flash('Login Successful. Welcome!', 'success')
             return redirect(next_page) if next_page else redirect(url_for('home.home_page'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
